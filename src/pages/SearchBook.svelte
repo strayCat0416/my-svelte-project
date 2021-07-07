084dfa43a16b4d4ff31c7f84d0c1e6acfcd2f333
strayCat0416 <saitoh.nespe@gmail.com>
2021-07-07 22:36:42 +0900

    子コンポーネントで受け取った値をバインディング

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import SearchBar from '../components/SearchBar.svelte'
+	let q = 'JavaSript'
 </script>
 
 <form>
-  <SearchBar />
+  <SearchBar bind:value={q} />
 </form>
+
+<div class="text-center mt-4">
+  { q }
+</div>
